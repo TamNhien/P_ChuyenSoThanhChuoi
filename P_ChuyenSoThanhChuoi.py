a9ae2e0142fd6ad6810389c82bf8cef13547d308
TamNhien <canhthien91@gmail.com>
2024-03-27 17:17:26 +0700

    Add project files.

---
+++ P_ChuyenSoThanhChuoi.py
@@ -0,0 +1,14 @@
+﻿def add_commas(num):
+    num_str = str(num)  
+    result = ""
+
+    for i, digit in enumerate(num_str[::-1]):
+        result = digit + result
+        if (i + 1) % 3 == 0 and i != len(num_str) - 1:
+            result = "," + result
+
+    return result
+
+num = int(input("Nhập số nguyên : "))
+print("Số nguyên với dấu phân cách hàng nghìn : ", add_commas(num))
+
